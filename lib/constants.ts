e66547b26bff301f7d0efce6f2bbc7df9a322253
ashrafrosli <mashrafr02@gmail.com>
2026-03-10 09:29:06 +0800

    NextJS Crash Course - Initial Commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    title: "React Summit 2026",
+    image: "/images/event1.png",
+    slug: "react-summit-2026",
+    location: "Amsterdam, Netherlands",
+    date: "2026-06-15",
+    time: "09:00"
+  },
+  {
+    title: "JSConf EU 2026",
+    image: "/images/event2.png",
+    slug: "jsconf-eu-2026",
+    location: "Berlin, Germany",
+    date: "2026-05-20",
+    time: "10:00"
+  },
+  {
+    title: "Vue.js London 2026",
+    image: "/images/event3.png",
+    slug: "vue-js-london-2026",
+    location: "London, UK",
+    date: "2026-07-10",
+    time: "11:00"
+  },
+  {
+    title: "Angular Connect 2026",
+    image: "/images/event4.png",
+    slug: "angular-connect-2026",
+    location: "London, UK",
+    date: "2026-09-25",
+    time: "09:30"
+  },
+  {
+    title: "Node.js Interactive 2026",
+    image: "/images/event5.png",
+    slug: "nodejs-interactive-2026",
+    location: "Austin, TX, USA",
+    date: "2026-11-12",
+    time: "08:00"
+  },
+  {
+    title: "PyCon US 2026",
+    image: "/images/event6.png",
+    slug: "pycon-us-2026",
+    location: "Pittsburgh, PA, USA",
+    date: "2026-04-22",
+    time: "09:00"
+  }
+];
